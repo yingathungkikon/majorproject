ef698b0146ecadcd6c1e81af111012239d6d57c6
yingathung <yingathungk@gmail.com>
2024-05-21 18:29:44 +0530

    initial commit with corrected composer.json

--- connection/connect.php
+++ connection/connect.php
@@ -1,13 +1,12 @@
 <?php
 
-    $servername = "sql300.infinityfree.com";
-    $username = "if0_36585469";
-    $password = "cXw3V442b4hlD2h";
-    $database = "if0_36585469_majorproject";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "majorproject";
     $conn = new mysqli($servername, $username, $password, $database);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    }
-
+    } 
 
 ?>
